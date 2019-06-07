e2211f8530e09f3a3e373bd478d1c400c6b880f5
tom_glb <goodMorning_glb@atguigu.com>
2019-06-08 07:11:37 +0800

    加入静态资源

--- src/main/java/com/hanghang/room/web/controller/HomeController.java
+++ src/main/java/com/hanghang/room/web/controller/HomeController.java
@@ -1,13 +1,14 @@
 package com.hanghang.room.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String index(){
+    public String index(Model model) {
+        model.addAttribute("name","muke");
         return "index";
     }
 }
