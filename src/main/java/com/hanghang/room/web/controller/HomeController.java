d3861608c246abefe4276f1ca9e135412f1c0fbe
tom_glb <goodMorning_glb@atguigu.com>
2019-06-08 08:21:56 +0800

    设置API，错误处理

--- src/main/java/com/hanghang/room/web/controller/HomeController.java
+++ src/main/java/com/hanghang/room/web/controller/HomeController.java
@@ -1,8 +1,10 @@
 package com.hanghang.room.web.controller;
 
+import com.hanghang.room.base.ApiResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -11,4 +13,39 @@ public class HomeController {
         model.addAttribute("name","muke");
         return "index";
     }
+
+
+    @GetMapping("/404")
+    public String notFoundPage() {
+        return "404";
+    }
+
+    @GetMapping("/403")
+    public String accessError() {
+        return "403";
+    }
+
+    @GetMapping("/500")
+    public String internalError() {
+        return "500";
+    }
+
+    @GetMapping("/logout/page")
+    public String logoutPage() {
+        return "logout";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
