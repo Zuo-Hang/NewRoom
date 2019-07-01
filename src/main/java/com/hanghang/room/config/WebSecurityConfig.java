5258fcfd16a145aea496641c27507d2ae45513a7
tom_glb <goodMorning_glb@atguigu.com>
2019-07-01 20:59:05 +0800

    权限控制，分别登陆

--- src/main/java/com/hanghang/room/config/WebSecurityConfig.java
+++ src/main/java/com/hanghang/room/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.hanghang.room.config;
 
 import com.hanghang.room.security.AuthProvider;
+import com.hanghang.room.security.LoginUrlEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,7 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
+@EnableWebSecurity  //允许使用security
 @EnableGlobalMethodSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -20,19 +21,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // 资源访问权限
+        // 资源访问权限分配
         http.authorizeRequests()
                 .antMatchers("/admin/login").permitAll() // 管理员登录入口
                 .antMatchers("/static/**").permitAll() // 静态资源
                 .antMatchers("/user/login").permitAll() // 用户登录入口
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/user/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/admin/**").hasRole("ADMIN")//管理员权限
+                .antMatchers("/user/**").hasAnyRole("ADMIN", "USER")//其他资源
                 .antMatchers("/api/user/**").hasAnyRole("ADMIN",
                 "USER")
-                .and()
+                .and()//以and结尾
                 .formLogin()
                 .loginProcessingUrl("/login") // 配置角色登录处理入口
-                .and();
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/logout/page")
+                .deleteCookies("JSESSIONID") //清除Cookies使得session失效
+                .invalidateHttpSession(true)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(urlEntryPoint())
+                .accessDeniedPage("/403");
         //使用同源策略
         http.csrf().disable();
         http.headers().frameOptions().sameOrigin();
@@ -46,9 +56,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         /*auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN").and();//内存模式认证*/
         auth.authenticationProvider(authProvider()).eraseCredentials(true);
     }
+    //注入自定义认证类
     @Bean
     public AuthProvider authProvider() {
         return new AuthProvider();
     }
-
+    //注入权限控制类类
+    @Bean
+    public LoginUrlEntryPoint urlEntryPoint() {
+        return new LoginUrlEntryPoint("/user/login");//默认到普通用户登陆界面
+    }
 }
